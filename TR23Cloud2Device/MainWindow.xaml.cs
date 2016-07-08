0d7b59441e2f73efc528dbe9cd3cb08d61bc83d6
rtiffany <rtiffany@MININT-LFF6ABV>
2016-07-08 14:57:42 -0700

    Add project files.

---
+++ TR23Cloud2Device/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Azure.Devices;
+
+
+namespace TR23Cloud2Device
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ServiceClient serviceClient;
+        string connectionString = "HostName=rt3IoTHub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=oANMWce9ymneEuKC4opJ6BodqBxzIlt95g5lyjbpgzQ=";
+        string message = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+        }
+
+        private async void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+
+            await SendCloudToDeviceMessageAsync();
+
+        }
+
+        private async Task SendCloudToDeviceMessageAsync()
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes(message));
+            await serviceClient.SendAsync("VendingMachineOne", commandMessage);
+        }
+
+
+    }
+}
